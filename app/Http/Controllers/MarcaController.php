2bab41c5081493a19f09e221bd33e7767a4dd894
joao <joao.pdesa@hotmail.com>
2023-05-06 00:45:45 -0300

    first commit

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+    private $marca;
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$marcas = Marca::all();
+
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //$marca = Marca::create($request->all());
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+        $image = $request->file('imagem');
+        $image_urn = $image->store('imagens', 'public');
+
+        $marca = $this->marca->create([
+            'nome' => $request->input('nome'),
+            'imagem' => $image_urn
+        ]);
+
+        return response()->json($marca, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            //return ['erro' => 'Recurso pesquisado não existe.'];
+            return response()->json(['erro' => 'Recurso pesquisado não existe.'], 404);
+        }
+
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marca $marca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe.'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+
+            foreach ($marca->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+
+        $marca->update($request->all());
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //$marca->delete();
+
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe.'], 404);
+        }
+
+        $marca->delete();
+        return response()->json(['msg' => 'A marca foi removida com sucesso!'], 200);
+    }
+}
---
+++ app/Models/Marca.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marca extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nome', 'imagem'];
+
+    public function rules()
+    {
+        return [
+            //'nome' => 'required|unique:marcas|min:3', // aqui estamos dizendo que o nome é único na tabela marcas
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png,jpg',
+        ];
+
+        /*
+            na linha 18 temos 3 parâmetros passados no unique para o caso de edição de alguma linha, que são:
+
+            1° - nome da tabela
+            2° - nome da coluna definida como unique
+            3° - o id que será ignorado na consulta por valores repetidos
+        */
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já existe',
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres',
+            'imagem.mimes' => 'O arquivo deve ser uma imagem do tipo PNG ou JPG'
+        ];
+    }
+}
